7ec0aa2d50c07906fb48bbcaf909371263abb807
Arman Karimi <mrevid2@gmail.com>
2022-12-22 02:00:26 +0330

    Implemented the whole lib

--- mix.exs
+++ mix.exs
@@ -31,8 +31,8 @@ defmodule OpenNotifyApi.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:jason, "~> 1.3"},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
     ]
   end
 end
